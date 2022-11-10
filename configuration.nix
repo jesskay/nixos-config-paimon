02fd056010e49317d4678e32c18c8b63d8bc5188
Jessica Kay <jesskay@psquid.net>
2022-11-10 23:10:19 +0000

    Force 80ms audio latency in discord

--- configuration.nix
+++ configuration.nix
@@ -101,7 +101,19 @@
         cfg.enableGnomeExtensions = true;
       })
       firefox-wayland
-      (discord.override {  # override to use the same nss as firefox
+      ((discord.overrideAttrs (super: {  # modify shortcut at the end of the install phase to force 80ms pulse latency
+        installPhase = (super.installPhase or "") + ''
+          # copy the content out of the symlinked applications dir so we can actually work with it
+          # (otherwise it's in a different derivation and perms won't let us touch it)
+          mkdir -p $out/share/applications-copy
+          cp -a $out/share/applications/* $out/share/applications-copy/
+          rm $out/share/applications
+          mv $out/share/applications-copy $out/share/applications
+
+          # patch the desktop item in place
+          sed 's/^Exec=/Exec=env PULSE_LATENCY_MSEC=200 /' -i $out/share/applications/discord.desktop
+          '';
+      })).override {  # override to use the same nss as firefox
         # will need updating if firefox ever uses a non-latest nss
         # obsoleted if/when https://github.com/NixOS/nixpkgs/pull/186603 lands
         nss = nss_latest;
