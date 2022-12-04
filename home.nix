8c3793209739b13b919bef1f66d0d9896a8ba5a2
Jessica Kay <jesskay@psquid.net>
2022-12-04 02:47:44 +0000

    Add mpv rotate keybinds

--- home.nix
+++ home.nix
@@ -12,10 +12,23 @@
 
   programs.mpv = {
     enable = true;
-    bindings = {
+    bindings = let
+      rotate0 = "rotate=angle=0";
+      rotate90 = "rotate=angle=PI/2:ow=ih:oh=iw";
+      rotate180 = "rotate=angle=PI";
+      rotate270 = "rotate=angle=-PI/2:ow=ih:oh=iw";
+    in {
+      # scaling
       "CTRL+-" = "add window-scale -0.125";
       "CTRL+0" = "set window-scale 1.0";
       "CTRL+=" = "add window-scale +0.125";
+      # rotation, on the numpad corners, clockwise from 1 (which is no rotation)
+      "KP1" = "no-osd set vf ${rotate0}";
+      "KP7" = "no-osd set vf ${rotate90}";
+      "KP9" = "no-osd set vf ${rotate180}";
+      "KP3" = "no-osd set vf ${rotate270}";
+      # and numpad 5 to cycle rotations (starting at 90 as at start there'll be no vf at all)
+      "KP5" = "no-osd cycle-values vf ${rotate90} ${rotate180} ${rotate270} ${rotate0}";
     };
   };
 }
