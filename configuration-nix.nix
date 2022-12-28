e1b7e8095aca736a5f2eedebb0de85d4b9a21d64
Jessica Kay <jesskay@psquid.net>
2022-12-28 23:13:52 +0000

    Add binary cache for devenv

--- configuration-nix.nix
+++ configuration-nix.nix
@@ -43,4 +43,10 @@
     options = "--delete-older-than 7d";
     dates = "weekly";
   };
+
+  # enable cachix cache for devenv
+  nix.settings = {
+    trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
+    substituters = [ "https://devenv.cachix.org" ];
+  };
 }
