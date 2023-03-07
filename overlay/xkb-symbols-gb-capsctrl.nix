655f4dbecc70b85d0afaa27c2f51f3b16fa41337
Jessica Kay <jesskay@psquid.net>
2023-03-07 21:08:53 +0000

    Move xkb symbols into a separate file

--- overlay/xkb-symbols-gb-capsctrl.nix
+++ overlay/xkb-symbols-gb-capsctrl.nix
@@ -7,17 +7,7 @@ stdenv.mkDerivation rec {
   pname = "xkb-symbols-gb-capsctrl";
   version = "1.0.0";
 
-  src = writeText "gb-capsctrl" ''
-    default partial alphanumeric_keys modifier_keys
-    xkb_symbols "basic" {
-        include "gb(basic)"
-
-        name[Group1]="English (UK, Caps is LCtrl)";
-
-        key <CAPS>      { [ Control_L, Control_L ] };
-        modifier_map  Control { <CAPS>, <LCTL> };
-    };
-  '';
+  src = ./gb-capsctrl.xkb;
 
   dontUnpack = true;
   dontBuild = true;
