9b3d51e4927e09c4bbe8fc0d54976d58138e6fe7
Jessica Kay <jesskay@psquid.net>
2023-03-13 21:20:22 +0000

    Switch to using unstable nixpkgs by default (adding module args to reference stable where desired)

--- flake.nix
+++ flake.nix
@@ -1,25 +1,40 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
-      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
     devenv.url = "github:cachix/devenv/v0.5";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs : let
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs : let
 
     system = "x86_64-linux";
 
+    pkgs-stable = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+
+    pkgs-unstable = import nixpkgs-unstable {
+      inherit system;
+      config.allowUnfree = true;
+    };
+
   in {
 
-    nixosConfigurations.paimon = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.paimon = nixpkgs-unstable.lib.nixosSystem {
       inherit system;
       modules =
         [ 
           ( { ... }: {   # Anonymous module to enable other modules to access flake inputs
             _module.args.inputs = inputs;
+          })
+
+          ( { ... }: {   # Anonymous module to inject a properly instantiated nixpkgs-stable into module args
+            _module.args.pkgs-stable = pkgs-stable;
           })
 
           # local modules
@@ -30,25 +45,19 @@
           ./configuration-yubikey.nix
           ./hardware-configuration.nix
           ./overlay
-
-          # upstream modules from a newer nixos (current module must be disabled below if present in both versions)
-          # : modules paths as strings: "${nixpkgs-unstable}/nixos/modules/path/to/module.nix"
-
-          # inline module to disable nixos-stable modules where required
-          # : list of paths relative to $REPO/nixos/modules/
-          (_: { disabledModules = [ ]; })
         ];
     };
 
     homeConfigurations.jess = home-manager.lib.homeManagerConfiguration {
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
+      pkgs = pkgs-unstable;
 
       modules = [
         ( { ... }: {   # Anonymous module to enable other modules to access flake inputs
           _module.args.inputs = inputs;
+        })
+
+        ( { ... }: {   # Anonymous module to inject a properly instantiated nixpkgs-stable into module args
+          _module.args.pkgs-stable = pkgs-stable;
         })
 
         # local modules
