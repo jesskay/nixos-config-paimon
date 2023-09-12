2ddd664a7cce8db3a953d4b4ee7d89bf78b11319
Jessica Kay <jesskay@psquid.net>
2023-09-12 21:03:19 +0100

    Refine borg paths and exclusions

--- configuration-borgbackup.nix
+++ configuration-borgbackup.nix
@@ -1,8 +1,18 @@
 { config, pkgs, lib, ... }:
 {
   services.borgbackup.jobs."paimon-data" = {
-    paths = [ "/home" "/media/win-e" ];
-    exclude = [ "/home/jess/Downloads" ];
+    paths = [ "/home/jess" ];
+    exclude = [
+      # exclude downloads as stuff only temporarily goes there
+      "/home/jess/Downloads"
+      # exclude onedrive as it's already synced
+      "/home/jess/OneDrive"
+      # exclude cache as anything there rapidly changes
+      "/home/jess/.cache"
+      # exclude steam and ffxiv data because those can be redownloaded from their servers instead
+      "/home/jess/.local/share/Steam/steamapps"
+      "/home/jess/.xlcore/ffxiv/game"
+    ];
     repo = "u360976@u360976.your-storagebox.de:paimon-data";
     encryption = {
       mode = "repokey-blake2";
