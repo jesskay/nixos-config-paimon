a8fb9674e2efb5722311c832ccb7ae59238b0464
Jessica Kay <jesskay@psquid.net>
2023-09-26 19:51:07 +0100

    Add hyprland substituter and key

--- configuration-nix.nix
+++ configuration-nix.nix
@@ -52,7 +52,13 @@
 
   # enable cachix cache for devenv
   nix.settings = {
-    trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
-    substituters = [ "https://devenv.cachix.org" ];
+    trusted-public-keys = [
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    ];
+    substituters = [
+      "https://devenv.cachix.org"
+      "https://hyprland.cachix.org"
+    ];
   };
 }
