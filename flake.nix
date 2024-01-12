75f442087fd6fa0240b081cd991f6a29221d1aa0
Jessica Kay <jesskay@psquid.net>
2024-01-12 20:19:00 +0000

    Remove nixpkgs-stable input (and registry entry) from flake as it was going unused, update inputs

--- flake.nix
+++ flake.nix
@@ -1,45 +1,38 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
     agenix = {
       url = "github:ryantm/agenix";
       inputs.home-manager.follows = "home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, agenix, ... }@inputs : let
+  outputs = { self, nixpkgs, home-manager, agenix, ... }@inputs : let
 
     system = "x86_64-linux";
 
-    pkgs-args-common = {
+    pkgs = import nixpkgs {
       inherit system;
-      config = {
-        allowUnfree = true;
-      };
+      config.allowUnfree = true;
     };
-
-    pkgs-stable = import nixpkgs pkgs-args-common;
-
-    pkgs-unstable = import nixpkgs-unstable pkgs-args-common;
 
   in {
 
-    nixosConfigurations.paimon = nixpkgs-unstable.lib.nixosSystem {
+    nixosConfigurations.paimon = nixpkgs.lib.nixosSystem {
       inherit system;
       modules =
         [ 
           ( { ... }: {   # Anonymous module to enable other modules to access flake inputs
             _module.args.inputs = inputs;
-          })
-
-          ( { ... }: {   # Anonymous module to inject a properly instantiated nixpkgs-stable into module args
-            _module.args.pkgs-stable = pkgs-stable;
           })
 
           # local modules
