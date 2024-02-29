e67fae6a5e8e4588c58682e152a9cabc75f72310
Jessica Kay <jesskay@psquid.net>
2024-02-29 22:16:48 +0000

    Revert "(fix) Apply blender cmake patch ahead of PR 291333 landing in unstable"
    This reverts commit 9e83d9a6e64ce189b66b846e77494bfa0925ec24.

--- overlay/default.nix
+++ overlay/default.nix
@@ -28,15 +28,6 @@
         ] ++ prev.mloader.propagatedBuildInputs;
       });
 
-      blender = prev.blender.overrideAttrs (super: {
-      	patches = prev.blender.patches ++ [
-	  (final.fetchpatch {
-	    url = "https://projects.blender.org/blender/blender/commit/cf4365e555a759d5b3225bce77858374cb07faad.diff";
-	    hash = "sha256-Nypd04yFSHYa7RBa8kNmoApqJrU4qpaOle3tkj44d4g=";
-	  })
-	];
-      });
-
       kitty = let
         kittyIcon256 = ./kitty.png;
 	kittyIcon128 = ./kitty-128.png;
