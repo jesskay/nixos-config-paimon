f5a8832f5d3b1e9f3e6060b39f459d9b2883f215
Jessica Kay <jesskay@psquid.net>
2024-06-02 04:54:53 +0100

    (nixos) Update fonts to use noto fonts as default serif and sans-serif, including CJK

--- configuration-fonts.nix
+++ configuration-fonts.nix
@@ -10,6 +10,9 @@
       roboto
       roboto-mono
       roboto-slab
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
       cabin
       overpass
       iosevka
@@ -18,7 +21,7 @@
 
       # fallbacks and "core" fonts
       # (enableDefaultFonts list as of 2022-12-18, but
-      #  with twitter emoji instead of noto, and MS corefonts and migu added)
+      #  with twitter emoji instead of noto, and MS corefonts added)
       dejavu_fonts
       freefont_ttf
       gyre-fonts  # TrueType substitutes for PostScript fonts
@@ -26,13 +29,22 @@
       unifont
       twitter-color-emoji
       corefonts  # Microsoft "Core" TrueType fonts
-      migu
     ];
 
     fontconfig = {
       defaultFonts = {
-        serif = [ "Ubuntu" ];
-        sansSerif = [ "Roboto Sans" "Migu" ];
+        serif = [
+	  "Noto Serif"
+	  "Noto Serif Japanese"
+	  "Noto Serif Korean"
+	  "Noto Serif Traditional Chinese"
+	];
+        sansSerif = [
+	  "Noto Sans"
+	  "Noto Sans Japanese"
+	  "Noto Sans Korean"
+	  "Noto Sans Traditional Chinese"
+	];
         monospace = [ "Roboto Mono" ];
         emoji = [ "Twitter Color Emoji" ];
       };
